4c2114df7dd2eeeeed477d5159a16320699f5359
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-01-06 00:57:19 -0500

    feat: config env

--- src/database.js
+++ src/database.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+
 mongoose
-  .connect('mongodb://localhost/test')
-  .then(() => console.log('connected'))
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DATABASE}?retryWrites=true&w=majority`
+  )
+  .then(() => console.log('DB connected'))
   .catch(err => console.log(err));
